87bb36ce59f599c4624c9129e5c7ff54b961c495
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-11-15 16:23:37 +0100

    detect import cycles

--- centjes/src/Centjes/Load.hs
+++ centjes/src/Centjes/Load.hs
@@ -5,27 +5,44 @@ import Centjes.Parse
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Logger
+import Control.Monad.State.Strict
 import qualified Data.ByteString as SB
+import Data.Set (Set)
+import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import Path
 import System.Exit
 
--- TODO keep track of modules that we've already loaded so we can error on
--- cycles instead of looping infinitely
 loadModules :: Path Abs File -> LoggingT IO [Declaration]
 loadModules firstPath = do
-  m <- readSingleModule firstPath
-  go firstPath m
+  flip evalStateT S.empty $ do
+    m <- readSingle firstPath
+    go firstPath m
   where
-    go :: Path Abs File -> Module -> LoggingT IO [Declaration]
+    go :: Path Abs File -> Module -> StateT (Set (Path Abs File)) (LoggingT IO) [Declaration]
     go p m = do
       let d = parent p
       restDecls <- forM (moduleImports m) $ \(Import rf) -> do
         let af = d </> rf
-        m' <- readSingleModule af
+        m' <- readSingle af
         go af m'
       pure $ concat (moduleDeclarations m : restDecls)
+
+    readSingle :: Path Abs File -> StateT (Set (Path Abs File)) (LoggingT IO) Module
+    readSingle p = do
+      visited <- get
+      if p `S.member` visited
+        then
+          liftIO $
+            die $
+              unlines
+                [ unwords ["Already loaded module at", fromAbsFile p],
+                  "This means there is an import cycle."
+                ]
+        else do
+          modify' (S.insert p)
+          lift $ readSingleModule p
 
 readSingleModule :: Path Abs File -> LoggingT IO Module
 readSingleModule fp = do
