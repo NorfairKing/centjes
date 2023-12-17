38b76cd53f1d9b68baa750e252be43ebb526a622
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-17 13:09:46 +0100

    scenario based balance tests

--- centjes-gen/test/Centjes/Report/BalanceSpec.hs
+++ centjes-gen/test/Centjes/Report/BalanceSpec.hs
@@ -14,6 +14,7 @@ import Centjes.Module
 import Centjes.Report.Balance
 import Centjes.Validation
 import Centjes.Validation.TestUtils
+import Control.Monad
 import Control.Monad.Logger
 import qualified Data.ByteString as SB
 import Data.Text (Text)
@@ -54,39 +55,19 @@ spec = do
       producesValid2
         (produceBalanceReport @())
 
-    tempDirSpec "centjes-balance-errors" $ do
-      it "balances this example transaction" $
-        moduleShouldBalance
-          Module
-            { moduleImports = [],
-              moduleDeclarations =
-                [ usdDeclaration,
-                  DeclarationTransaction $
-                    noLoc $
-                      Transaction
-                        { transactionTimestamp = noLoc (TimestampDay (fromGregorian 2023 12 17)),
-                          transactionDescription = Nothing,
-                          transactionPostings =
-                            [ noLoc
-                                Posting
-                                  { postingAccountName = noLoc (AccountName "assets"),
-                                    postingAccount = noLoc "1",
-                                    postingCurrencySymbol = noLoc usdSymbol,
-                                    postingCost = Nothing
-                                  },
-                              noLoc
-                                Posting
-                                  { postingAccountName = noLoc (AccountName "income"),
-                                    postingAccount = noLoc "-1",
-                                    postingCurrencySymbol = noLoc usdSymbol,
-                                    postingCost = Nothing
-                                  }
-                            ],
-                          transactionExtras = []
-                        }
-                ]
-            }
+    scenarioDir "test_resources/balance/balanced" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $
+        it "balances this module" $ do
+          -- Load the module
+          (ds, diag) <- runNoLoggingT $ loadModules af
+          -- Compile to a ledger
+          ledger <- shouldValidate diag $ compileDeclarations ds
 
+          br <- shouldValidate diag $ produceBalanceReport Nothing ledger
+          shouldBeValid br
+
+    tempDirSpec "centjes-balance-errors" $ do
       it "shows the same error when an account's total amount balance gets too large" $
         moduleGoldenBalanceError "test_resources/errors/balance-report/BE_RUNNING_BALANCE.err" $
           Module
@@ -497,23 +478,6 @@ spec = do
                         }
                 ]
             }
-
-moduleShouldBalance :: Module ann -> Path Abs Dir -> IO ()
-moduleShouldBalance m tdir = do
-  -- We have to write the module to a file to get the right source
-  -- locations to produce a nice error.
-  --
-  -- Write the module to a file
-  tfile <- resolveFile tdir "example-module.cent"
-  SB.writeFile (fromAbsFile tfile) (TE.encodeUtf8 (formatModule m))
-
-  -- Load the module
-  (ds, diag) <- runNoLoggingT $ loadModules tfile
-  -- Compile to a ledger
-  ledger <- shouldValidate diag $ compileDeclarations ds
-
-  br <- shouldValidate diag $ produceBalanceReport Nothing ledger
-  shouldBeValid br
 
 moduleGoldenBalanceError :: FilePath -> Module ann -> Path Abs Dir -> GoldenTest Text
 moduleGoldenBalanceError fp = moduleGoldenBalanceErrorHelper fp Nothing
