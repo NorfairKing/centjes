5ffc2c4c10a3e7c45ecf3ba3a4de242fd0c2481b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-19 16:03:53 +0100

    check errors via files

--- centjes-gen/test/Centjes/Command/CheckSpec.hs
+++ centjes-gen/test/Centjes/Command/CheckSpec.hs
@@ -1,30 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Centjes.Command.CheckSpec (spec) where
 
 import Centjes.Command.Check
-import Centjes.Format
 import Centjes.Load
 import Centjes.Location
 import Centjes.Module
 import Centjes.Module.Gen ()
 import Centjes.Validation
 import Centjes.Validation.TestUtils
+import Control.Monad
 import Control.Monad.Logger
-import qualified Data.ByteString as SB
 import Data.GenValidity.Containers ()
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.Map as M
-import Data.Text (Text)
-import qualified Data.Text.Encoding as TE
-import Data.Time
 import Path
 import Path.IO
 import Test.Syd
-import Test.Syd.Path
 import Test.Syd.Validity
 
 spec :: Spec
@@ -53,79 +47,13 @@ spec = do
           errs `shouldBe` CheckErrorUndeclaredAccount () () (Located () an) :| []
 
   describe "doCompleteCheck" $ do
-    tempDirSpec "centjes-check-errors" $ do
-      it "shows the same error when encountering a missing attachment" $
-        moduleGoldenCheckError "test_resources/errors/check/CE_MISSING_ATTACHMENT.err" $
-          Module
-            { moduleImports = [],
-              moduleDeclarations =
-                [ DeclarationTransaction $
-                    noLoc $
-                      Transaction
-                        { transactionTimestamp = noLoc (TimestampDay (fromGregorian 2023 12 05)),
-                          transactionDescription = Nothing,
-                          transactionPostings = [],
-                          transactionExtras =
-                            [ noLoc $ TransactionAttachment $ noLoc $ Attachment $ noLoc [relfile|example.pdf|]
-                            ]
-                        }
-                ]
-            }
-      it "shows the same error when encountering a duplicate account definition" $
-        moduleGoldenCheckError "test_resources/errors/check/CE_DUPLICATE_ACCOUNT.err" $
-          let ad = DeclarationAccount $ noLoc $ AccountDeclaration {accountDeclarationName = noLoc (AccountName "duplicate")}
-           in Module
-                { moduleImports = [],
-                  moduleDeclarations =
-                    [ ad,
-                      ad
-                    ]
-                }
-      it "shows the same error when encountering undeclared account" $
-        moduleGoldenCheckError "test_resources/errors/check/CE_UNDECLARED_ACCOUNT.err" $
-          let usdSymbol = CurrencySymbol "USD"
-              usdDeclaration =
-                DeclarationCurrency $
-                  noLoc $
-                    CurrencyDeclaration
-                      { currencyDeclarationSymbol = noLoc usdSymbol,
-                        currencyDeclarationQuantisationFactor = noLoc "0.01"
-                      }
-           in Module
-                { moduleImports = [],
-                  moduleDeclarations =
-                    [ usdDeclaration,
-                      DeclarationTransaction $
-                        noLoc $
-                          Transaction
-                            { transactionTimestamp = noLoc (TimestampDay (fromGregorian 2023 11 24)),
-                              transactionDescription = Nothing,
-                              transactionPostings =
-                                [ noLoc
-                                    Posting
-                                      { postingAccountName = noLoc (AccountName "undeclared"),
-                                        postingAccount = noLoc "1",
-                                        postingCurrencySymbol = noLoc usdSymbol,
-                                        postingCost = Nothing
-                                      }
-                                ],
-                              transactionExtras = []
-                            }
-                    ]
-                }
-
-moduleGoldenCheckError :: FilePath -> Module ann -> Path Abs Dir -> GoldenTest Text
-moduleGoldenCheckError file m tdir = do
-  goldenTextFile file $ do
-    -- We have to write the module to a file to get the right source
-    -- locations to produce a nice error.
-    --
-    -- Write the module to a file
-    tfile <- resolveFile tdir "example-module.cent"
-    SB.writeFile (fromAbsFile tfile) (TE.encodeUtf8 (formatModule m))
-
-    -- Load the module
-    (ds, diag) <- runNoLoggingT $ loadModules tfile
-    -- Try to check
-    errs <- shouldFailToValidateT $ doCompleteCheck ds
-    pure $ renderValidationErrors diag errs
+    scenarioDir "test_resources/check" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $ do
+        resultFile <- liftIO $ replaceExtension ".err" af
+        it "shows the same error when checking this module" $ do
+          goldenTextFile (fromAbsFile resultFile) $ do
+            (ds, diag) <- runNoLoggingT $ loadModules af
+            -- Try to check
+            errs <- shouldFailToValidateT $ doCompleteCheck ds
+            pure $ renderValidationErrors diag errs
