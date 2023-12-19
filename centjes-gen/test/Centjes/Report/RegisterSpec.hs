c5a9485c8849b86104b2a00e7091b110dc2648fa
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-19 15:44:53 +0100

    scenario-based register tests

---
+++ centjes-gen/test/Centjes/Report/RegisterSpec.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Centjes.Report.RegisterSpec (spec) where
+
+import Centjes.Command.Register (renderRegisterTable)
+import Centjes.Compile
+import Centjes.CurrencySymbol (CurrencySymbol (..))
+import Centjes.Ledger.Gen ()
+import Centjes.Load
+import Centjes.Report.Register
+import Centjes.Validation
+import Centjes.Validation.TestUtils
+import Control.Monad
+import Control.Monad.Logger
+import Path
+import Path.IO
+import Test.Syd
+import Test.Syd.Validity
+import Text.Colour
+
+spec :: Spec
+spec = do
+  describe "produceBalanceReport" $ do
+    it "produces valid reports" $
+      producesValid2
+        (produceRegister @())
+
+    scenarioDir "test_resources/register/valid/as-is" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $ do
+        reportFile <- liftIO $ replaceExtension ".txt" af
+        it "renders a register for this module the same way" $ do
+          goldenTextFile (fromAbsFile reportFile) $ do
+            -- Load the module
+            (ds, diag) <- runNoLoggingT $ loadModules af
+            -- Compile to a ledger
+            ledger <- shouldValidate diag $ compileDeclarations ds
+
+            br <- shouldValidate diag $ produceRegister Nothing ledger
+            shouldBeValid br
+            pure $ renderChunksText With24BitColours $ renderRegisterTable br
+
+    scenarioDir "test_resources/register/valid/to-chf" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $ do
+        reportFile <- liftIO $ replaceExtension ".txt" af
+        it "renders a register for this module the same way" $ do
+          goldenTextFile (fromAbsFile reportFile) $ do
+            -- Load the module
+            (ds, diag) <- runNoLoggingT $ loadModules af
+            -- Compile to a ledger
+            ledger <- shouldValidate diag $ compileDeclarations ds
+
+            br <- shouldValidate diag $ produceRegister (Just (CurrencySymbol "CHF")) ledger
+            shouldBeValid br
+            pure $ renderChunksText With24BitColours $ renderRegisterTable br
+
+    scenarioDir "test_resources/register/error/as-is" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $ do
+        resultFile <- liftIO $ replaceExtension ".err" af
+        it "shows the same error when trying to balance this module" $ do
+          goldenTextFile (fromAbsFile resultFile) $ do
+            -- Load the module
+            (ds, diag) <- runNoLoggingT $ loadModules af
+            -- Compile to a ledger
+            ledger <- shouldValidate diag $ compileDeclarations ds
+
+            errs <- shouldFailToValidate $ produceRegister Nothing ledger
+            pure $ renderValidationErrors diag errs
+
+    scenarioDir "test_resources/register/error/to-chf" $ \fp -> do
+      af <- liftIO $ resolveFile' fp
+      when (fileExtension af == Just ".cent") $ do
+        resultFile <- liftIO $ replaceExtension ".err" af
+        it "shows the same error when trying to balance this module" $ do
+          goldenTextFile (fromAbsFile resultFile) $ do
+            -- Load the module
+            (ds, diag) <- runNoLoggingT $ loadModules af
+            -- Compile to a ledger
+            ledger <- shouldValidate diag $ compileDeclarations ds
+
+            errs <- shouldFailToValidate $ produceRegister (Just (CurrencySymbol "CHF")) ledger
+            pure $ renderValidationErrors diag errs
