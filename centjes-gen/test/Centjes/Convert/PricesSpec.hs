61a8ba13d01f68fdec7f836b768aecc807e37599
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-12-29 22:17:18 +0100

    this doesnt work yet

--- centjes-gen/test/Centjes/Convert/PricesSpec.hs
+++ centjes-gen/test/Centjes/Convert/PricesSpec.hs
@@ -5,19 +5,19 @@ module Centjes.Convert.PricesSpec (spec) where
 import Centjes.Convert.Prices (Prices (..))
 import qualified Centjes.Convert.Prices as Prices
 import Centjes.Convert.Prices.Gen ()
-import Centjes.Ledger (Currency)
-import Centjes.Ledger.Gen ()
+import Centjes.CurrencySymbol
+import Centjes.CurrencySymbol.Gen ()
 import qualified Money.ConversionRate as ConversionRate
 import Test.Syd
 import Test.Syd.Validity
 
 spec :: Spec
 spec = do
-  genValidSpec @(Prices (Currency ()))
+  genValidSpec @(Prices String)
 
   describe "empty" $
     it "is valid" $
-      shouldBeValid (Prices.empty @(Currency ()))
+      shouldBeValid (Prices.empty @CurrencySymbol)
 
   describe "singleton" $ do
     it "is valid" $
@@ -25,12 +25,12 @@ spec = do
         forAllValid $ \to ->
           forAllValid $ \rate ->
             shouldBeValid
-              (Prices.singleton @(Currency ()) from to rate)
+              (Prices.singleton @CurrencySymbol from to rate)
     it "is the same as inserting one element" $
       forAllValid $ \from ->
         forAllValid $ \to ->
           forAllValid $ \rate ->
-            Prices.insert @(Currency ()) from to rate Prices.empty `shouldBe` Prices.singleton from to rate
+            Prices.insert @CurrencySymbol from to rate Prices.empty `shouldBe` Prices.singleton from to rate
 
   describe "insert" $
     it "produces valid values" $
@@ -38,44 +38,44 @@ spec = do
         forAllValid $ \to ->
           forAllValid $ \rate ->
             forAllValid $ \prices ->
-              shouldBeValid $ Prices.insert @(Currency ()) from to rate prices
+              shouldBeValid $ Prices.insert @CurrencySymbol from to rate prices
 
   describe "fromList" $
     it "produces valid prices" $
       producesValid $
-        Prices.fromList @(Currency ())
+        Prices.fromList @CurrencySymbol
 
   describe "lookupConversionFactor" $ do
     it "produces valid values" $
       forAllValid $ \from ->
         forAllValid $ \to ->
           forAllValid $ \prices ->
-            shouldBeValid $ Prices.lookupConversionFactor @(Currency ()) prices from to
+            shouldBeValid $ Prices.lookupConversionFactor @CurrencySymbol prices from to
 
     it "produces one for a currency and itself" $
       forAllValid $ \prices ->
         forAllValid $ \c ->
-          Prices.lookupConversionFactor @(Currency ()) prices c c `shouldBe` Just ConversionRate.oneToOne
+          Prices.lookupConversionFactor @CurrencySymbol prices c c `shouldBe` Just ConversionRate.oneToOne
 
     it "cannot convert anything with empty prices" $
       forAllValid $ \from ->
         forAllValid $ \to ->
           if from == to
             then pure ()
-            else Prices.lookupConversionFactor @(Currency ()) Prices.empty from to `shouldBe` Nothing
+            else Prices.lookupConversionFactor @CurrencySymbol Prices.empty from to `shouldBe` Nothing
 
     it "can find a single rate in one direction" $
       forAllValid $ \from ->
         forAllValid $ \to ->
           forAllValid $ \rate ->
-            Prices.lookupConversionFactor @(Currency ()) (Prices.singleton from to rate) from to
+            Prices.lookupConversionFactor @CurrencySymbol (Prices.singleton from to rate) from to
               `shouldBe` Just rate
 
     it "can find a single rate in the reverse direction" $
       forAllValid $ \from ->
         forAllValid $ \to ->
           forAllValid $ \rate ->
-            Prices.lookupConversionFactor @(Currency ()) (Prices.singleton from to rate) to from
+            Prices.lookupConversionFactor @CurrencySymbol (Prices.singleton from to rate) to from
               `shouldBe` Just (ConversionRate.invert rate)
 
     it "can a find a one-hop rate" $
@@ -83,6 +83,28 @@ spec = do
         forAllValid $ \c2 ->
           forAllValid $ \c3 ->
             forAllValid $ \r1 ->
-              forAllValid $ \r2 ->
+              forAllValid $ \r2 -> do
                 let ps = Prices.fromList [((c1, c2), r1), ((c2, c3), r2)]
-                 in Prices.lookupConversionFactor @(Currency ()) ps c1 c3 `shouldBe` Just (ConversionRate.compose r1 r2)
+                context (ppShow ps) $
+                  Prices.lookupConversionFactor @CurrencySymbol ps c1 c3 `shouldBe` Just (ConversionRate.compose r1 r2)
+
+    it "can a find a zero-hop rate even if multiple hops are available" $
+      forAllValid $ \c1 ->
+        forAllValid $ \c2 ->
+          forAllValid $ \c3 ->
+            forAllValid $ \r1 ->
+              forAllValid $ \r2 ->
+                forAllValid $ \r3 ->
+                  let ps = Prices.fromList [((c1, c2), r1), ((c2, c3), r2), ((c1, c3), r3)]
+                   in Prices.lookupConversionFactor @CurrencySymbol ps c1 c3 `shouldBe` Just r3
+
+    xit "can a find a two-hop rate" $
+      forAllValid $ \c1 ->
+        forAllValid $ \c2 ->
+          forAllValid $ \c3 ->
+            forAllValid $ \c4 ->
+              forAllValid $ \r1 ->
+                forAllValid $ \r2 ->
+                  forAllValid $ \r3 ->
+                    let ps = Prices.fromList [((c1, c2), r1), ((c2, c3), r2), ((c3, c4), r3)]
+                     in Prices.lookupConversionFactor @CurrencySymbol ps c1 c4 `shouldBe` Just (ConversionRate.compose r1 (ConversionRate.compose r2 r3))
--- centjes/src/Centjes/Convert/Prices.hs
+++ centjes/src/Centjes/Convert/Prices.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Centjes.Convert.Prices
   ( Prices (..),
@@ -18,6 +20,9 @@ import Centjes.Location
 import Data.Foldable
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Validity
 import GHC.Generics (Generic)
 import Money.ConversionRate as ConversionRate
@@ -35,10 +40,20 @@ data FromTo cur = FromTo !cur !cur
 
 instance Validity cur => Validity (FromTo cur)
 
-data Hops cur = HopFinal Money.ConversionRate
+data Hops cur
+  = -- This FromTo has a direct rate
+    HopFinal !Money.ConversionRate
+  | -- This FromTo has an indirect rate.
+    -- r_AC is composed from r_AB and r_BC
+    HopVia !cur !Money.ConversionRate
   deriving (Show, Eq, Generic)
 
 instance Validity cur => Validity (Hops cur)
+
+hopsRate :: Hops cur -> Money.ConversionRate
+hopsRate = \case
+  HopFinal r -> r
+  HopVia _ r -> r
 
 fromToInverted :: Ord a => a -> a -> Bool
 fromToInverted = (>)
@@ -55,24 +70,54 @@ mkFromTo' a1 a2 =
     then (ConversionRate.invert, FromTo a2 a1)
     else (id, FromTo a1 a2)
 
-class IsCurrency cur where
-  getQuantisationFactor :: cur -> Money.QuantisationFactor
-
-instance IsCurrency (Currency ann) where
-  getQuantisationFactor = locatedValue . currencyQuantisationFactor
+fromToSet :: Ord a => FromTo a -> Set a
+fromToSet (FromTo a1 a2) = S.fromList [a1, a2]
 
 empty :: Prices cur
 empty = Prices M.empty
 
 singleton :: Ord cur => cur -> cur -> Money.ConversionRate -> Prices cur
 singleton from to rate =
-  let (f, fromTo) = mkFromTo' from to
-   in Prices $ M.singleton fromTo (HopFinal (f rate))
+  if from == to
+    then empty
+    else
+      let (f, fromTo) = mkFromTo' from to
+       in Prices $ M.singleton fromTo (HopFinal (f rate))
 
-insert :: Ord cur => cur -> cur -> Money.ConversionRate -> Prices cur -> Prices cur
-insert from to rate (Prices m) =
-  let (f, fromTo) = mkFromTo' from to
-   in Prices $ M.insert fromTo (HopFinal (f rate)) m
+-- TODO precompute this?
+currencies :: Ord cur => Prices cur -> Set cur
+currencies (Prices m) = S.unions $ map fromToSet $ M.keys m
+
+insert :: forall cur. Ord cur => cur -> cur -> Money.ConversionRate -> Prices cur -> Prices cur
+insert from to rate p@(Prices m) =
+  if from == to
+    then p
+    else
+      let (f, fromTo@(FromTo from' to')) = mkFromTo' from to
+          directInserted = M.insert fromTo (HopFinal (f rate)) m
+          allCurrencies = S.toList $ currencies p
+
+          insertAll ::
+            [(FromTo cur, Hops cur)] ->
+            Map (FromTo cur) (Hops cur) ->
+            Map (FromTo cur) (Hops cur)
+          insertAll tups m = foldl' (\m' (ft, h) -> M.insert ft h m') m tups
+
+          -- Hops that arrive at the from'
+          singleHopsArriving :: [(FromTo cur, Hops cur)]
+          singleHopsArriving = do
+            c <- allCurrencies
+            let (arrivingF, arrivingFromTo) = mkFromTo' c from'
+            arrivingRate <- maybeToList $ arrivingF . hopsRate <$> M.lookup arrivingFromTo m
+            pure (arrivingFromTo, HopVia from' (ConversionRate.compose arrivingRate rate))
+          -- Hops that arrive at the from'
+          singleHopsDeparting :: [(FromTo cur, Hops cur)]
+          singleHopsDeparting = do
+            c <- allCurrencies
+            let (departingF, departingFromTo) = mkFromTo' to' c
+            departingRate <- maybeToList $ departingF . hopsRate <$> M.lookup departingFromTo m
+            pure (departingFromTo, HopVia to' (ConversionRate.compose rate departingRate))
+       in Prices $ insertAll singleHopsArriving $ insertAll singleHopsDeparting directInserted
 
 fromList :: Ord cur => [((cur, cur), Money.ConversionRate)] -> Prices cur
 fromList = foldl' (\ps ((c1, c2), r) -> insert c1 c2 r ps) empty
@@ -82,6 +127,5 @@ lookupConversionFactor (Prices m) from to
   | from == to = Just ConversionRate.oneToOne
   | otherwise = do
       let (f, fromTo) = mkFromTo' from to
-      mH <- M.lookup fromTo m
-      pure $ case mH of
-        HopFinal r -> f r
+      hops <- M.lookup fromTo m
+      pure $ f $ hopsRate hops
