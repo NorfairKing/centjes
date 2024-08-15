5d6114c8b7db71130866e5c9ac053905e5124743
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-08-15 06:58:21 +0200

    obsolete dependencies

--- centjes-docs-site/default.nix
+++ centjes-docs-site/default.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, aeson, autodocodec, autodocodec-yaml, autoexporter
-, base, bytestring, centjes, centjes-import-cornercard
-, centjes-import-neon, centjes-import-revolut, centjes-switzerland
-, cmark-gfm, containers, data-default, file-embed, filepath
-, fsnotify, lib, opt-env-conf, opt-env-conf-test, path, path-io
+{ mkDerivation, aeson, autoexporter, base, bytestring, centjes
+, centjes-import-cornercard, centjes-import-neon
+, centjes-import-revolut, centjes-switzerland, cmark-gfm
+, containers, data-default, file-embed, filepath, fsnotify, lib
+, opt-env-conf, opt-env-conf-test, path, path-io
 , safe-coloured-text, shakespeare, sydtest, sydtest-discover
 , template-haskell, template-haskell-reload, text
 , th-lift-instances, wai-extra, warp, yesod, yesod-autoreload
@@ -15,13 +15,13 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson autodocodec autodocodec-yaml base bytestring centjes
-    centjes-import-cornercard centjes-import-neon
-    centjes-import-revolut centjes-switzerland cmark-gfm containers
-    data-default file-embed filepath fsnotify opt-env-conf path path-io
-    safe-coloured-text shakespeare template-haskell
-    template-haskell-reload text th-lift-instances wai-extra warp yesod
-    yesod-autoreload yesod-sitemap yesod-static yesod-static-remote
+    aeson base bytestring centjes centjes-import-cornercard
+    centjes-import-neon centjes-import-revolut centjes-switzerland
+    cmark-gfm containers data-default file-embed filepath fsnotify
+    opt-env-conf path path-io safe-coloured-text shakespeare
+    template-haskell template-haskell-reload text th-lift-instances
+    wai-extra warp yesod yesod-autoreload yesod-sitemap yesod-static
+    yesod-static-remote
   ];
   libraryToolDepends = [ autoexporter ];
   executableHaskellDepends = [ base ];
