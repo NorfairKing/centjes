99bcd97317bd8faa0f3a4052868286230db46c77
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-04 16:14:02 +0200

    show income in tax report

--- centjes-switzerland/test/Centjes/Switzerland/Report/Taxes/Gen.hs
+++ centjes-switzerland/test/Centjes/Switzerland/Report/Taxes/Gen.hs
@@ -6,14 +6,30 @@ import Centjes.Ledger.Gen ()
 import Centjes.Switzerland.Report.Taxes.Types
 import Data.GenValidity
 import Data.GenValidity.Time ()
+import qualified Money.Account as Account
+import qualified Money.Amount as Amount
 import Money.Amount.Gen ()
+import Test.QuickCheck
 
 instance GenValid TaxesInput
 
 instance (Show ann, Ord ann, GenValid ann) => GenValid (TaxesReport ann) where
+  shrinkValid = shrinkValidStructurallyWithoutExtraFiltering
+  genValid = do
+    genValidStructurallyWithoutExtraChecking
+      `suchThatMap` ( \tr -> do
+                        totalAssets <- Account.sum (map assetAccountConvertedBalance (taxesReportAssetAccounts tr))
+                        pure $ tr {taxesReportTotalAssets = totalAssets}
+                    )
+      `suchThatMap` ( \tr -> do
+                        totalRevenues <- Amount.sum (map revenueAmount (taxesReportRevenues tr))
+                        pure $ tr {taxesReportTotalRevenues = totalRevenues}
+                    )
+
+instance (Show ann, Ord ann, GenValid ann) => GenValid (AssetAccount ann) where
   genValid = genValidStructurallyWithoutExtraChecking
   shrinkValid = shrinkValidStructurallyWithoutExtraFiltering
 
-instance (Show ann, Ord ann, GenValid ann) => GenValid (AssetAccount ann) where
+instance (Show ann, Ord ann, GenValid ann) => GenValid (Revenue ann) where
   genValid = genValidStructurallyWithoutExtraChecking
   shrinkValid = shrinkValidStructurallyWithoutExtraFiltering
